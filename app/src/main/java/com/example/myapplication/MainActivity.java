310cb85a5096ba36c26c785da6e9e0158ff51dc9
OULOUARK OUSSAMA <alioulouark@gmail.com>
2023-01-22 01:10:01 +0100

    Renitializer et quitter updated

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn_reinitializer,btn_quitter;
+    EditText number;
+    TextView textview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_reinitializer=findViewById(R.id.r);
+        btn_quitter=findViewById(R.id.q);
+        number=findViewById(R.id.insert);
+
+        btn_quitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        btn_reinitializer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number.setText(null);
+            }
+        });
     }
 }
